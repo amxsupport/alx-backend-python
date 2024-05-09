7c07cb0384c728428fd6ae35a886d2df4ac70965
amxsupport <itmasterdesigne@gmail.com>
2024-05-09 20:21:43 +0000

    create TestAccessNestedMap class

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Test utils
+"""
+import unittest
+from unittest.mock import patch
+import requests
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized, parameterized_class
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Access nested map """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path_map, result_expec):
+        """ Access nested method
+        """
+        self.assertEqual(access_nested_map(nested_map, path_map), result_expec)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path_map):
+        """ Exception access nested method
+        """
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path_map)
+
+        self.assertEqual(
+            f'KeyError({str(error.exception)})', repr(error.exception))
