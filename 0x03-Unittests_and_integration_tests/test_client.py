ce3513db795c73c345287ee27766ed45189edba4
amxsupport <itmasterdesigne@gmail.com>
2024-05-10 22:22:40 +0000

    create test_client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Unittests and integration tests """
+
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+from parameterized import parameterized, parameterized_class
+import json
+import unittest
+from unittest.mock import patch, PropertyMock, Mock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Class Testing Github Org Client """
+
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """Test that GithubOrgClient.org"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
+
